1198a57a685e8c41803abf93df89934344be9ab5
Jimmy <chen.jimmym@gmail.com>
2017-10-03 16:06:39 -0700

    done, hacker challenge also done

--- makingDictionaries.py
+++ makingDictionaries.py
@@ -1,10 +1,16 @@
 def make_dict(arr1, arr2):
-  new_dict = {}
-  temp = zip(arr1, arr2)
-  #print temp
-  new_dict = dict(temp)
-  print new_dict
-  return new_dict
+    new_dict = {}
+
+    if len(arr1) < len(arr2):
+        temp = arr1
+        arr1 = arr2
+        arr2 = temp
+
+    temp = zip(arr1, arr2)
+    #print temp
+    new_dict = dict(temp)
+    print new_dict
+    return new_dict
 
 name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
 favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
